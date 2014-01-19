33ed0bc7ec61f220587ac8151b120ed8a66aedc0
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-01-18 17:04:20 -0700

    close menu onwhen an item is clicked

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,4 +11,8 @@ $(document).ready(function(){
           window.location.hash = target;
       });
   });
+
+  $('.nav li a').on('click', function(){
+    $('.navbar-collapse.in').collapse('hide');
+  })
 });
